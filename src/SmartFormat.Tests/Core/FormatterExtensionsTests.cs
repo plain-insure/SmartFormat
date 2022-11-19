3131a7aba4e0deadea741544df424717c1f6bc2a
axunonb <nb@axuno.net>
2022-11-19 19:15:46 +0100

    Rename LogicCalcFormatter to LogiCalcFormatter
    * Change formatter name from "M" to "calc"
    * Change static variables of LogiCalcFormatter to ThreadStatic

--- src/SmartFormat.Tests/Core/FormatterExtensionsTests.cs
+++ src/SmartFormat.Tests/Core/FormatterExtensionsTests.cs
@@ -116,7 +116,6 @@ public class FormatterExtensionsTests
 
     #region: Custom Extensions :
 
-    [Test]
     [TestCase("{0}", 5, "TestExtension1 Options: , Format: ")]
     [TestCase("{0:N2}", 5, "TestExtension1 Options: , Format: N2")]
     public void Without_NamedFormatter_extensions_are_invoked_in_order(string format, object arg0, string expectedResult)
@@ -126,7 +125,6 @@ public class FormatterExtensionsTests
         Assert.That(actualResult, Is.EqualTo(expectedResult));
     }
 
-    [Test]
     [TestCase("{0:test1:}", 5, "TestExtension1 Options: , Format: ")]
     [TestCase("{0:test1():}", 5, "TestExtension1 Options: , Format: ")]
     [TestCase("{0:test1:N2}", 5, "TestExtension1 Options: , Format: N2")]
